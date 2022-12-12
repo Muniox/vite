f67a8a21efa2c70e259bec1f28bc5b5758958339
Muniox <muniox20@gmail.com>
2022-12-12 09:38:47 +0100

    my vite setup

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import postcssScss from 'postcss-scss'
+import autoprefixer from 'autoprefixer'
+import postcssNesting from 'postcss-nesting'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base: '',
+  css: {
+    devSourcemap: true,
+    postcss: {
+      map: true,
+      syntax: postcssScss,
+      plugins: [
+        autoprefixer({}),
+        postcssNesting
+      ]
+    }
+  },
+  plugins: [react()],
+})
